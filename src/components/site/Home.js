457ba9f60c890a50a5034e5f1d576de8c84a9af7
Jen Zimski <58517278+jlzimski@users.noreply.github.com>
2022-09-20 14:56:54 -0400

    update

--- src/components/site/Home.js
+++ src/components/site/Home.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import { card } from 'bootstrap'
 
 const Home = () => {
     return(
@@ -8,7 +9,7 @@ const Home = () => {
                 <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..."></img>
+      <img src='./assets/20220810_181117.jpg' class="card-img-top" alt="..."></img>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
